24313d7aac08782c34657868cc482690aa0e18d9
Quentin Selle <62110608+quentinselle@users.noreply.github.com>
2025-12-01 19:20:36 +0100

    Add day1 part1

--- src/day1.rs
+++ src/day1.rs
@@ -1,31 +1,88 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
+#[derive(Debug)]
+pub enum Direction{
+    Left{sub: isize},
+    Right{add: isize},
+}
+
 #[aoc_generator(day1)]
-fn parse(input: &str) -> String {
-    todo!()
+fn parse(input: &str) -> Vec<Direction>  {
+    input.lines().map(|l| {
+        let (direction, steps) = l.split_at(1);
+        if direction == "L" {
+            Direction::Left {sub: steps.parse().unwrap()}
+        } else {
+            Direction::Right {add: steps.parse().unwrap()}
+        }
+    }).collect()
 }
 
 #[aoc(day1, part1)]
-fn part1(input: &str) -> String {
-    todo!()
+fn part1(input: &[Direction]) -> usize {
+    let mut position = 50;
+    let mut count = 0;
+
+    for d in input {
+        match d {
+            Direction::Left { sub } => {
+                position -= sub;
+            }
+            Direction::Right { add } => {
+                position += add;
+            }
+        }
+        position = position.rem_euclid(100);
+        if position == 0 {
+            count+= 1;
+        }
+    }
+    count
 }
 
 #[aoc(day1, part2)]
-fn part2(input: &str) -> String {
-    todo!()
+fn part2(input: &[Direction]) -> isize {
+    let mut position = 50;
+    let mut count = 0;
+
+    for d in input {
+        match d {
+            Direction::Left { sub } => {
+                position -= sub;
+            }
+            Direction::Right { add } => {
+                position += add;
+            }
+        }
+        count += position.div_euclid(100).abs();
+        position = position.rem_euclid(100);
+
+    }
+    count
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const INPUT: &str = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
+
     #[test]
     fn part1_example() {
-        assert_eq!(part1(&parse("<EXAMPLE>")), "<RESULT>");
+        assert_eq!(part1(&parse(INPUT)), 3);
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+        assert_eq!(part2(&parse(INPUT)), 6);
     }
 }
